f8e44cf480655fd757a16497016fd1b231389bbb
Farhana <farhanna700@gmail.com>
2025-01-30 19:23:36 +0000

    improved the quality of the code based on the feedbacks

--- challengeFive.js
+++ challengeFive.js
@@ -1,48 +1,10 @@
 
-
-// First attempt 
-
-let cardNumber = "";
-
-function hideCardNumber(number){
-    const cardNumberString = number.toString();
-    const integers = cardNumberString.split("");
-    
-    for(let i = 0; i < integers.length - 4; i++) {
-        integers[i] = "*";
-        cardNumber += integers[i]
-    }
-   
-    for(let i = integers.length - 4; i < integers.length; i++){
-        cardNumber += integers[i]
-    }
-    console.log(cardNumber)
-}
-
-
-hideCardNumber(123838383848)
-
-
-
-// second attempt 
-
-let encryptedCardNumber = "";
-
-
-function hideCardNumber(number) {
-   const numberString = number.toString()
-   const newNum = numberString.split('')
-
-
-  const hideNumber = "*"
-  const hidedenNum = hideNumber.repeat(newNum.length -4)
-  encryptedCardNumber = hidedenNum
+function hideNumber(number){
+  cardNumber = number.toString();
+  const hiddenNumber = "*".repeat(cardNumber.length - 4)
+   return cardNumber.slice(- 4) + hiddenNumber
   
-  for(let i = newNum.length -4; i < newNum.length; i++){
-    encryptedCardNumber += newNum[i]
-  }
-  console.log(encryptedCardNumber)
 }
 
 
-hideCardNumber(12345678)
+console.log(hideNumber(1234567893838))
